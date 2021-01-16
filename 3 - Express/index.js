36a8e26c61d21730d95e4f5b03422fe16f476a32
Vinícius Vieira <vinicius.vdes@gmail.com>
2021-01-16 16:12:13 -0300

    Adicionando os arquivos do final do modulo de Express.

--- 3 - Express/index.js
+++ 3 - Express/index.js
@@ -8,12 +8,38 @@ app.get("/", function(req, res) {
     res.send("Bem vindo ao nosso site!!!");
 });
 
-app.get("/blog", function(req, res) {
-    res.send("Acessando o blog.");
+// app.get("/blog", function(req, res) {
+//     res.send("Acessando o blog.");
+// });
+
+app.get("/blog/:artigo?", function(req, res) {
+    let artigo = req.params.artigo;
+
+    if (artigo) {
+        res.send("<h2>id do artigo: " +artigo +".</h2>")
+    } else {
+        res.send("<h1>Bem vindo ao meu blog.</h1>");
+    }
+
 });
 
+// app.get("/canal/youtube", function(req, res) {
+//     res.send("Acessando o site do youtube.");
+// });
+
 app.get("/canal/youtube", function(req, res) {
-    res.send("Acessando o site do youtube.");
+    let canal = req.query["canal"];
+    if (canal) {
+        res.send(canal);        
+    } else {
+        res.send("My chanel.")
+    }
+
+});
+
+app.get("/ola/:nome", function(req, res) {
+    let nome = req.params.nome;
+    res.send("<h1>Olá, "+ nome +"</h1>");
 });
 
 app.listen(4000, function(erro) {
